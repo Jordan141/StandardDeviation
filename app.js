81e563bf9713fc53985e5e8112e72721ce986c3b
Jordan Moss <jmoss636@gmail.com>
2017-04-03 17:01:12 +0100

    Corrections and modularization

---
+++ app.js
@@ -0,0 +1,5 @@
+var stand_dev = require('./standard_deviation');
+
+let deviations = stand_dev.standard_deviation([26, 3, 5, 7, 8, 5, 0, 8, 4])
+
+console.log("Deviations:", deviations)
--- standard_deviation.js
+++ standard_deviation.js
@@ -1,20 +1,27 @@
+var exports = module.exports = {};
 
-function standard_deviation(inputArray){
+
+exports.standard_deviation = (inputArray) => {
 
 if(inputArray.length < 1) throw console.error("Standard Deviation Array Length must be 2 or more");
 
   let arrayAverage = arr => arr.reduce((sum, x) =>x + sum  ,0) / arr.length;
 
   let averageOrders = arrayAverage(inputArray)
+  console.log("Average Orders:", averageOrders)
 
   let differences = inputArray.map(x => x - averageOrders).map(x => x * x)
+  console.log("Differences:",differences);
 
   let variance = arrayAverage(differences)
+  console.log("Variances:",variance)
 
   let standardDeviation = Math.sqrt(variance)
 
-  return inputArray.map(x => x - averageOrders - standardDeviation > 0)
+  //Returns array of predicates on values of input array
+  let outLier =  inputArray.map( x => Math.abs(x - averageOrders) > standardDeviation)
+  console.log("Is outlier:",outLier)
+
+  return outLier
 
 }
-
-export default standard_deviation;
